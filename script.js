665a6c1baa7f4748286b95b2446b4b5c56528d00
ziauddin-0987 <ziauddink100@gmail.com>
2024-05-15 15:34:44 +0500

    add in @media qurey this folder in new file

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const navIcon = document.getElementById("nav_icon");
-
+const navBtn = document.getElementsByTagName("nav");
+let a = document.getElementsByClassName("nav_new_list");
 let show = 0;
 
 nav_icon.addEventListener("click", function () {
   if (show == 0) {
     console.log("on");
+    navBtn.classList.add(a);
     show = 1;
   } else if (show == 1) {
     console.log("off");
